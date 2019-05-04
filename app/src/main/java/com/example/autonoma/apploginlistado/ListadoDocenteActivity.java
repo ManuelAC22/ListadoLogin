e7ec30dafa5801cdc5c0aafd56027128b900b5f1
ManuelAC22 <mac2291998@gmail.com>
2019-05-04 12:05:16 -0500

    primer commit

---
+++ app/src/main/java/com/example/autonoma/apploginlistado/ListadoDocenteActivity.java
@@ -0,0 +1,53 @@
+package com.example.autonoma.apploginlistado;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ListadoDocenteActivity extends AppCompatActivity {
+
+    EditText txtNombreDocente ;
+    Button addDocente;
+    ListView listadoDocentes;
+
+    private ArrayList<String> datosDocente;
+    private ArrayAdapter<String> adaptador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_docente);
+
+        txtNombreDocente = findViewById(R.id.txtNombreDocente);
+        addDocente = findViewById(R.id.addDocente);
+        listadoDocentes = findViewById(R.id.listadoDocentes);
+
+        datosDocente = new ArrayList<String>();
+
+        adaptador=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datosDocente);
+        listadoDocentes.setAdapter(adaptador);
+
+        addDocente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!txtNombreDocente.getText().toString().equals("")) {
+                    datosDocente.add(txtNombreDocente.getText().toString());
+                    adaptador.notifyDataSetChanged();
+                    txtNombreDocente.setText("");
+                }else{
+                    Toast.makeText(ListadoDocenteActivity.this, "No puede ingresar un Docente Vacío", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/autonoma/apploginlistado/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.autonoma.apploginlistado;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtUsuario, txtPassword;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtUsuario = findViewById(R.id.txtUsuario);
+        txtPassword = findViewById(R.id.txtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String usuario = txtUsuario.getText().toString();
+                String password = txtPassword.getText().toString();
+
+                if(usuario.equals("admin") && password.equals("123456")){
+                    Intent i = new Intent(MainActivity.this, ListadoDocenteActivity.class);
+                    startActivity(i);
+                    finish();
+                }else{
+                    Toast.makeText(MainActivity.this, "Las credenciales no son las correctas", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
